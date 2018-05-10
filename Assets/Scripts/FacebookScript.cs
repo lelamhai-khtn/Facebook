9ffbb55269896789c0287922f79552d77041bfd4
lelamhai <leelamhair@gmail.com>
2018-05-11 00:26:35 +0700

    init facebook

---
+++ Assets/Scripts/FacebookScript.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using Facebook.Unity;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class FacebookScript : MonoBehaviour
+{
+
+    public Text FriendsText;
+    private Dictionary<string, string> profile = null;
+
+    private void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            FB.Init(() =>
+            {
+                if (FB.IsInitialized)
+                    FB.ActivateApp();
+                else
+                    Debug.LogError("Couldn't initialize");
+            },
+            isGameShown =>
+            {
+                if (!isGameShown)
+                    Time.timeScale = 0;
+                else
+                    Time.timeScale = 1;
+            });
+        }
+        else
+            FB.ActivateApp();
+    }
+
+    #region Login / Logout
+    public void FacebookLogin()
+    {
+        var permissions = new List<string>() { "public_profile", "email", "user_friends" };
+        FB.LogInWithReadPermissions(permissions, AuthCallback);
+    }
+    
+    void AuthCallback(IResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            FB.API("/me?fields=id,name", HttpMethod.GET, DealWithUserName);
+        }
+        else
+        {
+            
+        }
+    }
+
+    private void DealWithUserName(IGraphResult result)
+    {
+        if (result.Error != null)
+        {
+            Debug.Log("problem with getting profile picture");
+
+            FB.API("/me?fields=id,name", HttpMethod.GET, DealWithUserName);
+            return;
+        }
+
+        Debug.Log("\""+result.RawResult+"\"");
+        string jsonData = result.RawResult;
+        MyClass myObject = new MyClass();
+        myObject = JsonUtility.FromJson<MyClass>(jsonData);
+        Debug.Log(myObject.name);
+
+        //profile = Util.DeserializeJSONProfile(result.Text);
+
+        Text UserMsg = FriendsText.GetComponent<Text>();
+
+        UserMsg.text = "Hello, " + myObject.name;
+    }
+
+  
+
+    public void FacebookLogout()
+    {
+        FB.LogOut();
+    }
+
+
+    #endregion
+
+    public void FacebookShare()
+    {
+        FB.ShareLink(new System.Uri("http://resocoder.com"), "Check it out!",
+            "Good programming tutorials lol!",
+            new System.Uri("http://resocoder.com/wp-content/uploads/2017/01/logoRound512.png"));
+    }
+
+    #region Inviting
+    public void FacebookGameRequest()
+    {
+        FB.AppRequest("Hey! Come and play this awesome game!", title: "Reso Coder Tutorial");
+    }
+
+    public void FacebookInvite()
+    {
+        FB.Mobile.AppInvite(new System.Uri("https://play.google.com/store/apps/details?id=com.tappybyte.byteaway"));
+    }
+    #endregion
+
+    public void GetFriendsPlayingThisGame()
+    {
+        string query = "/me/friends";
+        FB.API(query, HttpMethod.GET, result =>
+        {
+            var dictionary = (Dictionary<string, object>)Facebook.MiniJSON.Json.Deserialize(result.RawResult);
+            var friendsList = (List<object>)dictionary["data"];
+            FriendsText.text = string.Empty;
+            foreach (var dict in friendsList)
+                FriendsText.text += ((Dictionary<string, object>)dict)["name"];
+        });
+    }
+}
---
+++ Assets/Scripts/MyClass.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+[Serializable]
+public class MyClass
+{
+    public string id;
+    public string name;
+}
